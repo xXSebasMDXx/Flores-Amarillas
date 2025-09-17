56cd03baf5cc71d9e6bc4f6d4167aa322af01b99
xXSebasMDXx <sebasmd1110@gmail.com>
2025-09-16 23:48:21 -0400

    Se agrega cueca Preso de tu mirada

--- anim.js
+++ anim.js
@@ -4,22 +4,29 @@ var lyrics = document.querySelector("#lyrics");
 
 // Array de objetos que contiene cada línea y su tiempo de aparición en segundos
 var lyricsData = [
-  { text: "At the time", time: 15 },
-  { text: "The whisper of birds", time: 18 },
-  { text: "Lonely before the sun cried", time: 27 },
-  { text: "Fell from the sky", time: 32 },
-  { text: "Like water drops", time: 33 },
-  { text: "Where I'm now? I don't know why", time: 41 },
-  { text: "Nice butterflies in my hands", time: 47 },
-  { text: "Too much light for twilight", time: 54 },
-  { text: "In the mood for the flowers love", time: 59 },
-  { text: "That vision", time: 67 },
-  { text: "Really strong, blew my mind", time: 72 },
-  { text: "Silence Let me see what it was", time: 78 },
-  { text: "I only want to live in clouds", time: 83 },
-  { text: "Where I'm now? I don't know why", time: 91 },
-  { text: "Nice butterflies in my hands", time: 97 },
-  { text: "Too much light for twilight", time: 104 },
+  { text: "Soy preso de tu mirada", time: 18 },
+  { text: "De esos tus labios de rosa", time: 20 },
+  { text: "Perdido en lo más profundo", time: 22 },
+  { text: "De esos tus luceros negros", time: 25 },
+  { text: "Deja que me salga el día", time: 28 },
+  { text: "Deja que muera mi noche", time: 30 },
+  { text: "Por si algún día me quisieras", time: 37 },
+  { text: "Déjame soñar contigo", time: 38 },
+  { text: "Deja que pueda alcanzarte", time: 40 },
+  { text: "Pañuelito palomita", time: 44 },
+  { text: "Soy preso de tu mirada", time: 47 },
+  { text: "De esos tus labios de rosa", time: 49 },
+
+  { text: "Tú, mi poema", time: 54 },
+  { text: "Mí, melodía", time: 56 },
+  { text: "Soy preso de tu mirada", time: 60 },
+  { text: "De esos tus labios de rosa", time: 62 },
+  { text: "Deja que me salga el día", time: 65 },
+  { text: "Deja que muera mi noche", time: 67 },
+
+  { text: "Deja que me salga el día", time: 85 },
+  { text: "Deja que muera mi noche", time: 86 },
+
   { text: "In the mood for the flowers love", time: 108 },
   { text: "At the time", time: 144 },
   { text: "The whisper of birds", time: 148 },
@@ -36,13 +43,14 @@ var lyricsData = [
 // Animar las letras
 function updateLyrics() {
   var time = Math.floor(audio.currentTime);
+  console.log(time);
   var currentLine = lyricsData.find(
     (line) => time >= line.time && time < line.time + 6
   );
 
   if (currentLine) {
     // Calcula la opacidad basada en el tiempo en la línea actual
-    var fadeInDuration = 0.1; // Duración del efecto de aparición en segundos
+    var fadeInDuration = 0.05; // Duración del efecto de aparición en segundos
     var opacity = Math.min(1, (time - currentLine.time) / fadeInDuration);
 
     // Aplica el efecto de aparición
